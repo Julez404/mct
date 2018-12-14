d11acc6cebde71ee4450d90ae8898f6cb41adc7d
Julian Linn <julian404@icloud.com>
2018-12-14 17:52:47 +0100

    Initial Commit

---
+++ timer/pwm/main.c
@@ -0,0 +1,18 @@
+/*
+ * pwm.c
+ *
+ * Created: 14.12.2018 17:46:04
+ * Author : julia
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
