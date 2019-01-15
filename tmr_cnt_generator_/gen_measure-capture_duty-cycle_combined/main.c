f4956cceb6a7a392cc593c326bb11d3486d70c22
Julian Linn <julian404@icloud.com>
2019-01-15 17:42:47 +0100

    Implementation of Dutycycle meassurement
    Testet for all Input Frequencys except of prescaler "1". Arduino can't handle prescaler, most likely due to a stackoverflow caused by the Interrupt length of Timer1-CPT.

--- tmr_cnt_generator_/gen_measure-capture_duty-cycle_combined/main.c
+++ tmr_cnt_generator_/gen_measure-capture_duty-cycle_combined/main.c
@@ -1,4 +1,5 @@
-// #define ARDUINO_IDE
+//#define ARDUINO_IDE
+#define SET_DUTY_CYCLE 0.20
 // #define PRESCALER_0
 // #define PRESCALER_1
 // #define PRESCALER_8
@@ -22,6 +23,9 @@ void initTmr(void);
 
 volatile uint8_t tmr1_ovf_count = 0;
 volatile double period_us = 0;
+volatile double period_high_us = 0;
+volatile double period_low_us = 0;
+
 
 void setup()
 {
@@ -51,18 +55,25 @@ int main(void)
 		Serial.print("F: ");
 		Serial.print(1/period);
 		Serial.println(" Hz");
+		Serial.print("Duty: ");
+		Serial.println(period_high_us/period_us);
+		Serial.print("T_on: ");
+		Serial.print(period_high_us);
+		Serial.println(" us");
+		Serial.print("T_off: ");
+		Serial.print(period_low_us);
+		Serial.println(" us");
 		Serial.println();
 		#endif
-		_delay_ms(250);
+		_delay_ms(500);
 	}
 }
 
 void initPort(void)
 {
 	DDRB &= ~(1 << 0);  //PB0 Input
+	DDRB |= (1 << 3); //PD7 Output
 	PORTB = 0;
-	DDRD |= (1 << 7); //PD7 Output
-	PORTD = 0;
 
 	#ifdef ARDUINO_IDE
 	Serial.println("Ports Configured");
@@ -73,43 +84,35 @@ void initTmr(void)
 {
 	//Init Timer 1 for Signal Capture
 	TCCR1A = 0;
-	TCCR1B = (1 << 7) | (1 << 6) | (1 << CS10);
+	TCCR1B = (1 << 7) | (1 << 6) | (1 << CS11);
 	TCCR1C = 0;
-	TIMSK1 = (1<<ICIE1) | (1 << TOIE1);
 
 	#ifdef ARDUINO_IDE
 	Serial.println("Timer1 initialized");
 	#endif
 
 	//Init Timer 2 for output Signal
-	TCCR2A = 0;
+	TCCR2A = (1 << WGM21) | (1 << WGM20) | (1 << COM2A0) | (1 << COM2A1);
 	TCCR2B = (PRE_BIT_2 << 2) |(PRE_BIT_1 << 1)|(PRE_BIT_0 << 0);
-	TIMSK2 |= (1 << 0);
+	OCR2A = (int)(SET_DUTY_CYCLE * 255);
 	
 	#ifdef ARDUINO_IDE
 	Serial.println("Timer2 initialized");
 	#endif
 	
 	sei();
-
 	#ifdef ARDUINO_IDE
 	Serial.println("Interrupts activ!");
 	#endif
 }
 
-ISR(TIMER2_OVF_vect)
-{
-	PORTD ^= (1 << 7);
-}
-
 
 ISR(TIMER1_CAPT_vect)
 {
-	
 	uint16_t timestamp = (ICR1L) | (ICR1H << 8);
 	uint32_t period_count = 0;
 	static uint16_t timestamp_old;
-
+	
 	if(tmr1_ovf_count == 0)
 	{
 		period_count = timestamp-timestamp_old;
@@ -126,7 +129,18 @@ ISR(TIMER1_CAPT_vect)
 		period_count+= (65536-timestamp_old);
 	}
 
-	period_us = period_count * 0.5;//0.0625 for prescaler=1
+	if(TCCR1B & ICES1)
+	{
+		period_low_us = period_count * 0.5;	//0.0625 for prescaler=1
+	}
+	else
+	{
+		period_high_us = period_count * 0.5;	//0.0625 for prescaler=1
+	}
+
+	period_us = period_low_us + period_high_us;
+	//period_count * 0.5;//0.0625 for prescaler=1
+
 	timestamp_old = timestamp;
 	tmr1_ovf_count=0;
 }
